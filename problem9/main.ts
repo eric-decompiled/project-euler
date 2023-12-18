c81d68513d6de607bdb6999777f3a7fb1a640550
eric-decompiled <eric@decompiled.dev>
2023-12-18 09:42:28 -0500

    Update problem9/main.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- problem9/main.ts
+++ problem9/main.ts
@@ -1,14 +1,20 @@
-const main = () => {
-  const sum = 1000;
+const findPythagoreanTripletProduct = (sum) => {
   for (let a = 1; a < sum; a++) {
     for (let b = a + 1; b < sum; b++) {
       let c = sum - a - b;
-
       if (a ** 2 + b ** 2 === c ** 2) {
-        console.log({ a, b, c }, "Product: ", a * b * c);
-        return;
+        return a * b * c;
       }
     }
+  }
+  return null;
+};
+
+const main = () => {
+  const sum = 1000;
+  const product = findPythagoreanTripletProduct(sum);
+  if (product) {
+    console.log("Product: ", product);
   }
 };
 

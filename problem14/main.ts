2bb993435f98cbb27ce66b5a64bdfacce04ff86d
Eric Richardson <eric@decompiled.dev>
2024-01-06 15:57:51 -0500

    solve problem 14

---
+++ problem14/main.ts
@@ -0,0 +1,26 @@
+const collazt = (num: number): number => {
+  let steps = 0;
+  while (num !== 1) {
+    steps++;
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
+    }
+  }
+
+  return steps;
+};
+
+let mostSteps = 0;
+let mostStart = 0;
+for (let i = 1; i < 1000000; i++) {
+  const steps = collazt(i);
+
+  if (steps > mostSteps) {
+    mostSteps = steps;
+    mostStart = i;
+  }
+}
+
+console.log({ mostStart, mostSteps });

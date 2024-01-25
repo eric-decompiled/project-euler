ed1b25b69095ecac247107e0ef45a3f9e26d8155
Eric Richardson <eric@decompiled.dev>
2024-01-25 08:00:54 -0500

    solve problem 22

---
+++ problem22/main.ts
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const data = fs.readFileSync(`${__dirname}/names.txt`, "utf-8");
+
+const nameValues: number[][] = data
+  .split(",")
+  .sort()
+  .map((n: string) =>
+    n
+      .replace(/"/g, "")
+      .split("")
+      .map((c) => c.charCodeAt(0) - 64)
+  );
+
+console.log(nameValues);
+
+let answer = 0;
+
+for (const [index, values] of nameValues.entries()) {
+  const sum = values.reduce((acc, n) => acc + n);
+  answer += sum * (index + 1);
+}
+
+console.log(answer);
